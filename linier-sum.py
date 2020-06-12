5b8c149ee12f09ec8cd4c2aac32112fbe157ff52
Pradip Gareja <pradipgareja@Pradips-MacBook-Air.local>
2020-06-13 00:35:13 +0530

    system m

--- linier-sum.py
+++ linier-sum.py
@@ -4,3 +4,9 @@ def linear_recursion(S,n):
     else:
         return linear_recursion(S,n-1)_S[n-1]
 
+def reverse(S, start, stop):
+”””Reverse elements in implicit slice S[start:stop].”””
+if start < stop − 1:
+S[start], S[stop−1] = S[stop−1], S[start] reverse(S, start+1, stop−1)
+# if at least 2 elements: # swap first and last
+# recur on rest
